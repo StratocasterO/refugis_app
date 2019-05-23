afc7b744e09f978f3083a75bba32a1df7c670de0
StratocasterO <omarolmedoferrer@gmail.com>
2019-05-23 14:21:43 +0200

    añadida pagina add hut

--- scripts/javascript_home.js
+++ scripts/javascript_home.js
@@ -2,16 +2,29 @@ const pulsarRojo=_=>{
     console.log('llego');
     const cajaRoja = document.querySelector('#menu');
     const cajaGrande = document.querySelector('#m_contenedor');
+    const menuDespl = document.querySelector('#menu2');
+    
     cajaGrande.classList.toggle('invisible');
+    menuDespl.style.display='none';
 }
 
 window.addEventListener('click', function(e){ 
     const m_contenedor = document.querySelector('#m_contenedor'); 
     const menu = document.querySelector('#menu2');
-
+    
     if ( !m_contenedor.contains(e.target)){
-         m_contenedor.classList.remove('invisible');
+        m_contenedor.classList.remove('invisible');
+        menu.style.display='block';
     }
 });
 
-document.querySelector('#menu2').addEventListener('click',pulsarRojo);
+
+document.querySelector('.menu2').addEventListener('click',pulsarRojo);
+
+const pulsarMas=_=>{
+    console.log('llego');
+    const cajaGrande = document.querySelector('#añadir');
+    
+    location.href="nuevo.html"
+}
+document.querySelector('#añadir').addEventListener('click',pulsarMas);
